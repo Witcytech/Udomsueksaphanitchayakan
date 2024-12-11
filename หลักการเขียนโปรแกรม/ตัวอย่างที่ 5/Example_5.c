d39f3181bb1d2f44526dd332352c6f3d38eeb31c
RoxyX <taou8022@gmail.com>
2024-12-11 20:12:34 +0700

    Update Example_5.c

--- หลักการเขียนโปรแกรม/ตัวอย่างที่ 5/Example_5.c
+++ หลักการเขียนโปรแกรม/ตัวอย่างที่ 5/Example_5.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main() {
+    int price, discount, saleprice;
+    
+    printf("Input Price : ");
+    scanf("%d", &price);
+    
+    discount = price * 0.15;
+    saleprice = price - discount;
+    
+    printf("SalePrice = %d", saleprice);
+    
+    return 0;
+}
 
+// หมายเหตุ: สิ่งนี้ทำเพื่อใช้ในการศึกษาและแบ่งปันให้ผู้อื่นสามารถนำไปประยุกต์ใช้ได้ (การเขียนโปรแกรมโดยใช้ภาษา C) ซึ่งเป็นส่วนหนึ่งของวิชาหลักการเขียนโปรแกรม
