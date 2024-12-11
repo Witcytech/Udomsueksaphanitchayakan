19f9b2db3b8426a4e474ac6c7860620e11a9d34b
RoxyX <taou8022@gmail.com>
2024-12-11 20:09:35 +0700

    Update Example_3.c

--- หลักการเขียนโปรแกรม/ตัวอย่างที่ 3/Example_3.c
+++ หลักการเขียนโปรแกรม/ตัวอย่างที่ 3/Example_3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+int main() {
+    int price, piece, totalprice;
+    
+    printf("Input Price : ");
+    scanf("%d", &price);
+    
+    printf("Input Piece : ");
+    scanf("%d", &piece);
+    
+    totalprice = price * piece;
+    
+    printf("TotalPrice = %d", totalprice);
+    
+    return 0;
+}
 
+// หมายเหตุ: สิ่งนี้ทำเพื่อใช้ในการศึกษาและแบ่งปันให้ผู้อื่นสามารถนำไปประยุกต์ใช้ได้ (การเขียนโปรแกรมโดยใช้ภาษา C) ซึ่งเป็นส่วนหนึ่งของวิชาหลักการเขียนโปรแกรม
