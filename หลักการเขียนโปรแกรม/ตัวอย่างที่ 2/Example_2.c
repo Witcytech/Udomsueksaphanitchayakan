296e653e56f7034083f2d86348853b8f61f757b4
RoxyX <taou8022@gmail.com>
2024-12-11 20:09:06 +0700

    Update Example_2.c

--- หลักการเขียนโปรแกรม/ตัวอย่างที่ 2/Example_2.c
+++ หลักการเขียนโปรแกรม/ตัวอย่างที่ 2/Example_2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+int main() {
+    int width, length, area;
+    
+    printf("Input Width : ");
+    scanf("%d", &width);
+    
+    printf("Input Length : ");
+    scanf("%d", &length);
+    
+    area = width * length;
+    
+    printf("FootballArea = %d", area);
+    
+    return 0;
+}
 
+// หมายเหตุ: สิ่งนี้ทำเพื่อใช้ในการศึกษาและแบ่งปันให้ผู้อื่นสามารถนำไปประยุกต์ใช้ได้ (การเขียนโปรแกรมโดยใช้ภาษา C) ซึ่งเป็นส่วนหนึ่งของวิชาหลักการเขียนโปรแกรม
