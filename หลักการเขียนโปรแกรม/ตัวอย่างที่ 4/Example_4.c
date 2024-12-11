74c92702a690dead82be57a6adddda6d35cef90a
RoxyX <taou8022@gmail.com>
2024-12-11 20:11:25 +0700

    Update Example_4.c

--- หลักการเขียนโปรแกรม/ตัวอย่างที่ 4/Example_4.c
+++ หลักการเขียนโปรแกรม/ตัวอย่างที่ 4/Example_4.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int main() {
+    int salary, hour, totalot, totalsalary;
+    
+    printf("Input Salary : ");
+    scanf("%d", &salary);
+    
+    printf("Input Hour : ");
+    scanf("%d", &hour);
+    
+    totalot = hour * 200;
+    totalsalary = salary + totalot;
+    
+    printf("TotalSalary = %d", totalsalary);
+    
+    return 0;
+}
 
+// หมายเหตุ: สิ่งนี้ทำเพื่อใช้ในการศึกษาและแบ่งปันให้ผู้อื่นสามารถนำไปประยุกต์ใช้ได้ (การเขียนโปรแกรมโดยใช้ภาษา C) ซึ่งเป็นส่วนหนึ่งของวิชาหลักการเขียนโปรแกรม
