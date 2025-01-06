5732380d71b90b509da7ce3971f1030a59be1682
RoxyX <taou8022@gmail.com>
2025-01-06 19:36:01 +0700

    Update Ex_3.cpp

--- การเขียนโปรแกรมภาษาคอมพิวเตอร์/3/Ex_3.cpp
+++ การเขียนโปรแกรมภาษาคอมพิวเตอร์/3/Ex_3.cpp
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
 
+int main() {
+    int pen;
+    int price_pen = 10;
+    int book;
+    int price_book = 200;
+    int total_pen;
+    int total_book;
+
+    printf("Enter your Pen = ");
+    scanf("%d", &pen);
+
+    printf("Enter your Book = ");
+    scanf("%d", &book);
+
+    total_pen = pen * price_pen;
+    printf("Total Pen = %d \n", total_pen);
+
+    total_book = book * total_book;
+    printf("Total Book = %d", total_book);
+
+    return 0;
+}
+
+// คำเตือน: สิ่งนี้สร้างขึ้นมาเพื่อแบ่งปันให้ทุกคนนำไปใช้ประโยชน์ได้อย่างอิสระ โดยมีวัตถุประสงค์เพื่อการศึกษาและการต่อยอด (การเขียนโปรแกรมโดยใช้ภาษา C++) ซึ่งเป็นส่วนหนึ่งของวิชาการเขียนโปรแกรมภาษาคอมพิวเตอร์
